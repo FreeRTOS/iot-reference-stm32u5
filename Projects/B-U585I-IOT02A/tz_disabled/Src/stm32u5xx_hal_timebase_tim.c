d430859651d64d0dad56948083ddacd2a9932fbf
Paul Bartell <pbartell@amazon.com>
2021-07-22 15:33:21 -0700

    Update from latest cubemx release

--- Projects/B-U585I-IOT02A/tz_disabled/Src/stm32u5xx_hal_timebase_tim.c
+++ Projects/B-U585I-IOT02A/tz_disabled/Src/stm32u5xx_hal_timebase_tim.c
@@ -50,6 +50,7 @@ HAL_StatusTypeDef HAL_InitTick(uint32_t TickPriority)
 
   /* Enable the TIM6 global Interrupt */
   HAL_NVIC_EnableIRQ(TIM6_IRQn);
+
   /* Enable TIM6 clock */
   __HAL_RCC_TIM6_CLK_ENABLE();
 
@@ -74,6 +75,7 @@ HAL_StatusTypeDef HAL_InitTick(uint32_t TickPriority)
   htim6.Init.Prescaler = uwPrescalerValue;
   htim6.Init.ClockDivision = 0;
   htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
+
   if(HAL_TIM_Base_Init(&htim6) == HAL_OK)
   {
     /* Start the TIM time Base generation in interrupt mode */
