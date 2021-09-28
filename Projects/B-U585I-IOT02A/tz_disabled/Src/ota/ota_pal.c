e99c48874c177d920dc5c6b69ff2d71a004cbb87
Ravishankar Bhagavandas <bhagavar@amazon.com>
2021-09-28 14:33:30 -0700

    Updates to ota pal

--- Projects/B-U585I-IOT02A/tz_disabled/Src/ota/ota_pal.c
+++ Projects/B-U585I-IOT02A/tz_disabled/Src/ota/ota_pal.c
@@ -39,29 +39,36 @@
 
 const char OTA_JsonFileSignatureKey[ OTA_FILE_SIG_KEY_STR_MAX_LENGTH ] = "sig-sha256-ecdsa";
 
+#define FLASH_START_CURRENT_BANK       ( ( uint32_t ) FLASH_BASE )
 
-#define FLASH_START_CURRENT_BANK       ((uint32_t)FLASH_BASE)
-
-#define FLASH_START_ALT_BANK           ((uint32_t)(FLASH_BASE + FLASH_BANK_SIZE ))
+#define FLASH_START_PASSIVE_BANK           (( uint32_t )( FLASH_BASE + FLASH_BANK_SIZE ))
 
 #define NUM_QUAD_WORDS( length )       ( length >> 4UL )
 
 #define NUM_REMAINING_BYTES( length )  ( length & 0x0F )
 
-#define IMAGE_CONTEXT_FILE_NAME        "/ota_metadata"
+#define ALTERNATE_BANK( bank )         ( ( bank == FLASH_BANK_1 ) ? FLASH_BANK_2 : FLASH_BANK_1 )
+
+#define IMAGE_CONTEXT_FILE_NAME        "/ota/"
+
+typedef enum OtaPalImageStateInternal
+{
+	OTA_PAL_IMAGE_STATE_UNKNOWN = 0,
+	OTA_PAL_IMAGE_STATE_PROGRAMMING,
+	OTA_PAL_IMAGE_STATE_PENDING_COMMIT,
+	OTA_PAL_IMAGE_STATE_COMMITTED,
+	OTA_PAL_IMAGE_STATE_INVALID
+} OtaPalImageStateInternal_t;
 
 typedef struct OtaImageContext
 {
-	uint32_t bank;
+	uint32_t ulBank;
 	uint32_t ulBaseAddress;
 	uint32_t ulImageSize;
-	OtaPalImageState_t state;
-
+	OtaPalImageStateInternal_t state;
 } OtaImageContext_t;
 
 static FLASH_OBProgramInitTypeDef    OBInit;
-
-static OtaImageContext_t imageContext;
 
 static BaseType_t prvLoadImageContextFromFlash( OtaImageContext_t *pContext )
 {
@@ -101,6 +108,21 @@ static BaseType_t prvLoadImageContextFromFlash( OtaImageContext_t *pContext )
 #endif
 
 }
+
+
+static OtaImageContext_t * prvGetImageContext( void )
+{
+	static OtaImageContext_t imageContext = { 0 };
+
+    if( imageContext.state == OTA_PAL_IMAGE_STATE_UNKNOWN )
+	{
+    	( void ) prvLoadImageContextFromFlash( &imageContext );
+	}
+
+    return &imageContext;
+}
+
+
 
 static BaseType_t prvSaveImageContextToFlash( OtaImageContext_t * pContext )
 {
@@ -256,7 +278,7 @@ static HAL_StatusTypeDef prvSwapBankAndBoot( void )
 	return status;
 }
 
-static uint32_t prvGetAlternateBank( void )
+static uint32_t prvGetActiveBank( void )
 {
 	HAL_StatusTypeDef status = HAL_ERROR;
 	uint32_t ulBank = 0UL;
@@ -269,7 +291,7 @@ static uint32_t prvGetAlternateBank( void )
 		{
 			/* Get the Dual boot configuration status */
 			HAL_FLASHEx_OBGetConfig( &OBInit );
-			ulBank = ( ( OBInit.USERConfig &  OB_SWAP_BANK_ENABLE ) == OB_SWAP_BANK_ENABLE ) ? FLASH_BANK_1 : FLASH_BANK_2;
+			ulBank = ( ( OBInit.USERConfig &  OB_SWAP_BANK_ENABLE ) == OB_SWAP_BANK_ENABLE ) ? FLASH_BANK_2 : FLASH_BANK_1;
 
 			( void ) HAL_FLASH_OB_Lock();
 
@@ -279,8 +301,14 @@ static uint32_t prvGetAlternateBank( void )
 	}
 
 	return ulBank;
-
 }
+
+static uint32_t prvGetPassiveBank( void )
+{
+	uint32_t activeBank = prvGetActiveBank();
+	return ALTERNATE_BANK( activeBank );
+}
+
 
 static HAL_StatusTypeDef prvWriteToFlash(uint32_t destination, uint8_t * pSource, uint32_t length)
 {
@@ -382,32 +410,28 @@ OtaPalStatus_t xOtaPalCreateImage( OtaFileContext_t * const pFileContext )
 {
 	OtaPalStatus_t otaStatus = OtaPalRxFileCreateFailed;
 	HAL_StatusTypeDef status = HAL_ERROR;
-	uint32_t ulOtherBank;
-
-	/* Try to load an image context if one exists in the flash. */
-	( void ) prvLoadImageContextFromFlash( &imageContext );
+	uint32_t ulPassiveBank = prvGetPassiveBank();
+	OtaImageContext_t * pContext = prvGetImageContext();
 
 	if( ( pFileContext->pFile == NULL ) &&
 			( pFileContext->fileSize <= FLASH_BANK_SIZE ) &&
-			( imageContext.state != OtaPalImageStatePendingCommit ) )
+			( pContext->state != OTA_PAL_IMAGE_STATE_PROGRAMMING ) )
 	{
 		/* Set dual bank mode if not already set. */
 		status = prvFlashSetDualBankMode();
-
 		if( status == HAL_OK )
 		{
 			/* Get Alternate bank and erase the flash. */
-			ulOtherBank = prvGetAlternateBank();
-			status = prvEraseBank( ulOtherBank );
+			status = prvEraseBank( ulPassiveBank );
 		}
 
 		if( status == HAL_OK )
 		{
-			imageContext.bank = ulOtherBank;
-			imageContext.ulBaseAddress = FLASH_START_ALT_BANK;
-			imageContext.ulImageSize = pFileContext->fileSize;
-			imageContext.state = OtaPalImageStateUnknown;
-			pFileContext->pFile = ( uint8_t * ) ( &imageContext );
+			pContext->ulBank = ulPassiveBank;
+			pContext->ulBaseAddress = FLASH_START_PASSIVE_BANK;
+			pContext->ulImageSize = pFileContext->fileSize;
+			pContext->state = OTA_PAL_IMAGE_STATE_PROGRAMMING;
+			pFileContext->pFile = ( uint8_t * ) pContext;
 			otaStatus = OtaPalSuccess;
 		}
 	}
@@ -422,11 +446,13 @@ int16_t iOtaPalWriteImageBlock ( OtaFileContext_t * const pFileContext,
 {
 	int16_t bytesWritten = 0;
 	HAL_StatusTypeDef status = HAL_ERROR;
+	OtaImageContext_t * pContext = prvGetImageContext();
 
-	if( ( pFileContext->pFile == ( uint8_t *) ( &imageContext ) ) &&
-			( offset + blockSize ) <= imageContext.ulImageSize )
+	if( ( pFileContext->pFile == ( uint8_t *) ( pContext ) ) &&
+		( ( offset + blockSize ) <= pContext->ulImageSize )  &&
+		( pContext->state == OTA_PAL_IMAGE_STATE_PROGRAMMING ) )
 	{
-		status = prvWriteToFlash( ( imageContext.ulBaseAddress + offset ), pData, blockSize );
+		status = prvWriteToFlash( ( pContext->ulBaseAddress + offset ), pData, blockSize );
 		if( status == HAL_OK )
 		{
 			bytesWritten = blockSize;
@@ -439,11 +465,14 @@ int16_t iOtaPalWriteImageBlock ( OtaFileContext_t * const pFileContext,
 OtaPalStatus_t xOtaPalFinalizeImage( OtaFileContext_t * const pFileContext )
 {
 	OtaPalStatus_t otaStatus = OtaPalCommitFailed;
+	OtaImageContext_t * pContext = prvGetImageContext();
 
-	if( pFileContext->pFile == ( uint8_t *) ( &imageContext ) )
+	if( ( pFileContext->pFile == ( uint8_t *) ( pContext ) ) &&
+		( pContext->state == OTA_PAL_IMAGE_STATE_PROGRAMMING ) )
+
 	{
 		/* To do signature validation and confirm image here. */
-		imageContext.state = OtaPalImageStatePendingCommit;
+		pContext->state = OTA_PAL_IMAGE_STATE_PENDING_COMMIT;
 		otaStatus = OtaPalSuccess;
 	}
 
@@ -454,16 +483,23 @@ OtaPalStatus_t xOtaPalFinalizeImage( OtaFileContext_t * const pFileContext )
 OtaPalStatus_t xOtaPalAbortImage( OtaFileContext_t * const pFileContext )
 {
 	OtaPalStatus_t otaStatus = OtaPalAbortFailed;
-	HAL_StatusTypeDef status = HAL_ERROR;
+	HAL_StatusTypeDef status = HAL_OK;
+	OtaImageContext_t *pContext = prvGetImageContext();
 
+	if( pFileContext->pFile == ( uint8_t *) ( pContext ) )
 
-	if( ( pFileContext->pFile == ( uint8_t *) ( &imageContext ) ) &&
-			( imageContext.bank == prvGetAlternateBank() ) )
 	{
-		status = prvEraseBank( imageContext.bank );
-		if( status == HAL_OK )
+		if( ( pContext->state != OTA_PAL_IMAGE_STATE_UNKNOWN ) &&
+			( pContext->ulBank == prvGetPassiveBank() ) )
 		{
-			imageContext.state = OtaPalImageStateUnknown;
+			status = prvEraseBank( pContext->ulBank );
+			if( status == HAL_OK )
+			{
+				otaStatus = OtaPalSuccess;
+			}
+		}
+		else
+		{
 			otaStatus = OtaPalSuccess;
 		}
 	}
@@ -475,19 +511,19 @@ OtaPalStatus_t xOtaPalActivateImage( OtaFileContext_t * const pFileContext )
 {
 	OtaPalStatus_t otaStatus = OtaPalActivateFailed;
 	BaseType_t status = pdFALSE;
+	OtaImageContext_t *pContext = prvGetImageContext();
 
-	if( ( imageContext.state  == OtaPalImageStatePendingCommit ) &&
-		( imageContext.bank   == prvGetAlternateBank() ) )
+	if( ( pContext->state == OTA_PAL_IMAGE_STATE_PENDING_COMMIT ) &&
+		( pContext->ulBank == prvGetPassiveBank() ) )
 	{
-		/** Save current image context to flash so as to get the context after boot. */
-		status = prvSaveImageContextToFlash( &imageContext );
+		/** Save current image context to flash so as to fetch the context after boot. */
+		status = prvSaveImageContextToFlash( pContext );
 
 		if( status == pdTRUE )
 		{
 			( void )  prvSwapBankAndBoot();
 
 			/** Boot failed. Removed the save context from flash. */
-
 			( void ) prvRemoveImageContextFromFlash();
 		}
 
@@ -500,28 +536,18 @@ OtaPalStatus_t xOtaPalSetImageState( OtaFileContext_t * const pFileContext,
 		OtaImageState_t eState )
 {
 	OtaPalStatus_t otaStatus = OtaPalBadImageState;
-	BaseType_t status = pdPASS;
-	uint32_t alternateBank;
+	OtaImageContext_t * pContext = prvGetImageContext();
+	uint32_t activeBank = prvGetActiveBank();
+	uint32_t passiveBank = ALTERNATE_BANK( activeBank );
 
-	if( imageContext.state == OtaPalImageStateUnknown )
-	{
-		/*
-		 * Function is called before starting a new OTA image download. This can occur
-		 * either we have booted with the new image in pending commit, or due to an error from
-		 * the upper layer (there wont be an OTA image context in flash). Try loading the context
-		 * from flash and return error if not found.
-		 */
-		status = prvLoadImageContextFromFlash( &imageContext );
-	}
-
-	if( status == pdPASS )
+	if( pContext->state != OTA_PAL_IMAGE_STATE_UNKNOWN )
 	{
 		switch( eState )
 		{
 		case OtaImageStateTesting:
 
-			if( ( imageContext.state == OtaPalImageStatePendingCommit ) &&
-					( imageContext.bank != prvGetAlternateBank() ) )
+			if( ( pContext->state == OTA_PAL_IMAGE_STATE_PENDING_COMMIT ) &&
+				( pContext->ulBank == activeBank ) )
 			{
 				/** New image bank is booted successfully and it's pending for commit. */
 				otaStatus = OtaPalSuccess;
@@ -529,50 +555,64 @@ OtaPalStatus_t xOtaPalSetImageState( OtaFileContext_t * const pFileContext,
 			break;
 
 		case OtaImageStateAccepted:
-			alternateBank = prvGetAlternateBank();
-			if( ( imageContext.state == OtaPalImageStatePendingCommit ) &&
-					( imageContext.bank != alternateBank ) )
+			if( ( pContext->state == OTA_PAL_IMAGE_STATE_PENDING_COMMIT ) &&
+				( pContext->ulBank == activeBank ) )
 			{
 				/** New image bank is booted successfully and it have passed self test. Make it as accepted
 				 * by removing the image context from flash and setting image state to valid. */
 
 				prvRemoveImageContextFromFlash();
-				( void ) prvEraseBank( alternateBank );
-				imageContext.state = OtaPalImageStateValid;
+
+				( void ) prvEraseBank( passiveBank );
+				pContext->state = OTA_PAL_IMAGE_STATE_COMMITTED;
 				otaStatus = OtaPalSuccess;
+			}
+			else
+			{
+				otaStatus = OtaPalCommitFailed;
 			}
 			break;
 
 		case OtaImageStateRejected:
-			if( ( imageContext.state == OtaPalImageStatePendingCommit ) &&
-					( imageContext.bank != prvGetAlternateBank() ) )
+
+			/* Make sure the image is not committed already. A committed image cannot be aborted or rejected. */
+			if( pContext->state != OTA_PAL_IMAGE_STATE_COMMITTED )
 			{
+				/*
+				 *  Remove the persisted state of image from flash.
+				 */
 				prvRemoveImageContextFromFlash();
-				( void ) prvSwapBankAndBoot();
-
-			}
-			otaStatus = OtaPalRejectFailed;
-			break;
-
-		case OtaImageStateAborted:
-			if( imageContext.bank != prvGetAlternateBank() )
-			{
-				/* We are booting from the new bank and the image is pending commit. */
-				if( imageContext.state == OtaPalImageStatePendingCommit )
-				{
-					prvRemoveImageContextFromFlash();
-					( void ) prvSwapBankAndBoot();
-					otaStatus = OtaPalAbortFailed;
-				}
+				pContext->state = OTA_PAL_IMAGE_STATE_INVALID;
+				otaStatus = OtaPalSuccess;
 			}
 			else
 			{
-				otaStatus = xOtaPalAbortImage( pFileContext );
+				otaStatus = OtaPalRejectFailed;
 			}
+
+			break;
+
+		case OtaImageStateAborted:
+
+			/* Make sure the image is not committed already. A committed image cannot be aborted or rejected. */
+			if( pContext->state != OTA_PAL_IMAGE_STATE_COMMITTED )
+			{
+				/*
+				 *  Remove the persisted state of image from flash.
+				 */
+				prvRemoveImageContextFromFlash();
+				pContext->state = OTA_PAL_IMAGE_STATE_INVALID;
+				otaStatus = OtaPalSuccess;
+			}
+			else
+			{
+				otaStatus = OtaPalAbortFailed;
+			}
+
 			break;
 
 		default:
-			LogError(( "Unknown state transition, current state = %d, expected state = %d.\r\n",imageContext.state, eState ));
+			LogError(( "Unknown state transition, current state = %d, expected state = %d.\r\n",pContext->state, eState ));
 			break;
 		}
 	}
@@ -585,21 +625,24 @@ OtaPalStatus_t xOtaPalSetImageState( OtaFileContext_t * const pFileContext,
 OtaPalImageState_t xOtaPalGetImageState( OtaFileContext_t * const pFileContext )
 {
 	OtaPalImageState_t state = OtaPalImageStateUnknown;
-	BaseType_t status = pdPASS;
+	OtaImageContext_t * pContext = prvGetImageContext();
 
-	if(imageContext.state == OtaPalImageStateUnknown )
+	switch( pContext->state )
 	{
-		/*
-		 * API can be called before starting a new OTA image download. This can occur
-		 * when OTA image enters self test mode after activation of new image.
-		 * Try to get the state of the image stored in flash.
-		 */
-		status = prvLoadImageContextFromFlash( &imageContext );
-	}
-
-	if( status == pdPASS )
-	{
-		state = imageContext.state;
+	case OTA_PAL_IMAGE_STATE_PENDING_COMMIT:
+		state = OtaPalImageStatePendingCommit;
+		break;
+	case OTA_PAL_IMAGE_STATE_COMMITTED:
+		state = OtaPalImageStateValid;
+		break;
+	case OTA_PAL_IMAGE_STATE_INVALID:
+		state = OtaPalImageStateInvalid;
+		break;
+	case OTA_PAL_IMAGE_STATE_PROGRAMMING:
+	case OTA_PAL_IMAGE_STATE_UNKNOWN:
+	default:
+		state = OtaPalImageStateUnknown;
+		break;
 	}
 
 	return state;
@@ -608,6 +651,21 @@ OtaPalImageState_t xOtaPalGetImageState( OtaFileContext_t * const pFileContext )
 
 OtaPalStatus_t xOtaPalResetDevice( OtaFileContext_t * const pFileContext )
 {
-	/* TODO: Implement this function. */
+
+	OtaImageContext_t * pContext = prvGetImageContext();
+	uint32_t activeBank = prvGetActiveBank();
+
+	if( ( pContext->state != OTA_PAL_IMAGE_STATE_COMMITTED ) &&
+		( pContext->ulBank == activeBank ) )
+	{
+		prvSwapBankAndBoot();
+	}
+	else
+	{
+		/*
+		 * Just Boot.
+		 */
+	}
+
 	return OtaPalUninitialized;
 }
