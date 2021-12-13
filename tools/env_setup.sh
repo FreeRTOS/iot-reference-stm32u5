b4c16244fc07cf1c07addba0f6b99dda146c866d
Paul Bartell <pbartell@amazon.com>
2021-12-13 12:12:41 -0800

    Fix env_setup.sh script for use on windows.

--- tools/env_setup.sh
+++ tools/env_setup.sh
@@ -1,24 +1,75 @@
 #!/bin/bash
+#
+#  FreeRTOS STM32 Reference Integration
+#
+#  Copyright (C) 2021 Amazon.com, Inc. or its affiliates.  All Rights Reserved.
+#
+#  Permission is hereby granted, free of charge, to any person obtaining a copy of
+#  this software and associated documentation files (the "Software"), to deal in
+#  the Software without restriction, including without limitation the rights to
+#  use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+#  the Software, and to permit persons to whom the Software is furnished to do so,
+#  subject to the following conditions:
+#
+#  The above copyright notice and this permission notice shall be included in all
+#  copies or substantial portions of the Software.
+#
+#  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+#  FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+#  COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+#  IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+#  CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#
+#  https://www.FreeRTOS.org
+#  https://github.com/FreeRTOS
+#
+#
+if [ -n "${BASH_SOURCE[0]}" ]; then
+	TOOLS_PATH=$( realpath "$(cd "$(dirname "${BASH_SOURCE[0]}")" || return; pwd )" )
+else
+	TOOLS_PATH=$( realpath "$(cd "$(dirname "${0}")" || return; pwd )" )
+fi
+echo "TOOLS_PATH: ${TOOLS_PATH}"
 
-which python3 || alias python3=python
+VENV_PATH=$(realpath "${TOOLS_PATH}/..")/.venv
+echo "VENV_PATH:  ${VENV_PATH}"
+echo
+
+which python > /dev/null 2>&1 || alias python=python3
+which python > /dev/null 2>&1 || {
+	echo "Failed to find a validate python installation."
+	return 1
+}
 
 load_venv()
 {
-	if [ -e ${PWD}/.venv/Scripts ]; then
-		source ${PWD}/.venv/Scripts/activate
-	elif [ -e ${PWD}/.venv/bin ]; then
-		source ${PWD}/.venv/bin/activate
+	if [ -e "${VENV_PATH}"/Scripts ]; then
+		source "${VENV_PATH}"/Scripts/activate
+	elif [ -e "${VENV_PATH}"/bin ]; then
+		source "${VENV_PATH}"/bin/activate
 	else
 		echo "Error. Could not find python virtual environment activation script."
-		exit -1
+		return 1
 	fi
+
+	echo "Adding tools directory to path."
+	export PATH="${TOOLS_PATH}:${PATH}"
 }
 
-[ -e ${PWD}/.venv ] || {
+if [ ! -d "${VENV_PATH}" ]; then
 	echo "Setting up python virtual environment"
-	python3 -m virtualenv ${PWD}/.venv || exit -1
-	load_venv || exit -1
-	pip3 install -r $(dirname "$0")/requirements.txt || rm -rf ${PWD}/.venv
-}
+	python -m virtualenv "${VENV_PATH}" || return 1
 
-load_venv || exit -1
+	echo "Activating vritual environment."
+	load_venv || return 1
+
+	pip3 install -r "${TOOLS_PATH}"/requirements.txt || {
+		rm -rf "${VENV_PATH}"
+		echo "Error while installing requried python packages. Removing incomplete virtual environment."
+		return 1
+	}
+else
+	load_venv || return 1
+fi
+echo "Done."
--- tools/provision.py
+++ tools/provision.py
@@ -1,4 +1,4 @@
-#!python3
+#!python
 #
 #  FreeRTOS STM32 Reference Integration
 #
