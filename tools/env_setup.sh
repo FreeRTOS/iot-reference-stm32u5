17b7dccd60eda64556d43f9586cebedab2a63f54
Paul Bartell <pbartell@amazon.com>
2021-12-13 10:47:03 -0800

    Update env_setup script to use virtualenv and be compatible with windows.

--- tools/env_setup.sh
+++ tools/env_setup.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-[ -e ${PWD}/.pyenv ] || {
-	echo "Setting up python virtual environment"
-	python3 -m venv ${PWD}/.pyenv || exit -1
+which python3 || alias python3=python
+
+load_venv()
+{
+	if [ -e ${PWD}/.venv/Scripts ]; then
+		source ${PWD}/.venv/Scripts/activate
+	elif [ -e ${PWD}/.venv/bin ]; then
+		source ${PWD}/.venv/bin/activate
+	else
+		echo "Error. Could not find python virtual environment activation script."
+		exit -1
+	fi
 }
 
-source ${PWD}/.pyenv/bin/activate
+[ -e ${PWD}/.venv ] || {
+	echo "Setting up python virtual environment"
+	python3 -m virtualenv ${PWD}/.venv || exit -1
+	load_venv || exit -1
+	pip3 install -r $(dirname "$0")/requirements.txt || rm -rf ${PWD}/.venv
+}
 
-pip3 install -r $(dirname "$0")/requirements.txt
-
+load_venv || exit -1
