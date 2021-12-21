ad4803c38a63998abdb8266e498c6b531852143b
Paul Bartell <pbartell@amazon.com>
2021-12-21 11:39:40 -0800

    Update do_rdp_regression.sh and flash_device.sh

--- Projects/b_u585i_iot02a_tfm/scripts/do_rdp_regression.sh
+++ Projects/b_u585i_iot02a_tfm/scripts/do_rdp_regression.sh
@@ -26,41 +26,27 @@
 #
 #
 
+# CUBEIDE_DIR must be provided by the caller as environment variables
+
+[ -z ${CUBEIDE_DIR} ] && {
+    echo "Error: CUBEIDE_DIR must be defined to continue."
+    exit 1
+}
+
 PROG_DIR=`find ${CUBEIDE_DIR} -name 'com.st.stm32cube.ide.mcu.externaltools.cubeprogrammer*' -type d`
 PROG_BIN_DIR="${PROG_DIR}/tools/bin"
 PROG_BIN=`find ${PROG_BIN_DIR} -name 'STM32_Programmer_CLI*' -type f`
 PROG_BIN=$( basename ${PROG_BIN} )
-TARGET_BIN_DIR=${PWD}
-
 
 [ -e "${PROG_BIN_DIR}/${PROG_BIN}" ] || {
     echo "Could not determine STM32CubeProgrammer location"
-    exit -1
-}
-
-[ -e ${TARGET_BIN_DIR} ] || {
-    echo "Project binary dir could not be determined."
-    exit -1
-}
-
-source ${TARGET_BIN_DIR}/image_defs.sh
-
-[ -z "${secbootadd0}" ] && {
-    echo "Error: Could not determine target secbootadd0."
-    exit -1
+    exit 1
 }
 
 echo "STM32CubeIDE directory:               ${CUBEIDE_DIR%?}"
 echo "STM32CubeProgrammer Path:             ${PROG_BIN_DIR}"
 echo "STM32CubeProgrammer Bin Name:         ${PROG_BIN}"
 echo; echo
-echo "RE_BL2_BOOT_ADDRESS:                  ${secbootadd0}"
-echo "RE_BL2_PERSO_ADDRESS:                 ${boot}"
-echo "RE_IMAGE_FLASH_ADDRESS_SECURE:        ${slot0}"
-echo "RE_IMAGE_FLASH_ADDRESS_NON_SECURE:    ${slot1}"
-echo "RE_IMAGE_FLASH_SECURE_UPDATE:         ${slot2}"
-echo "RE_IMAGE_FLASH_NON_SECURE_UPDATE:     ${slot3}"
-echo
 
 export PATH="${PROG_BIN_DIR}:${PATH}"
 
@@ -69,11 +55,11 @@ ${PROG_BIN} -c port=SWD mode=HotPlug -ob RDP=0xAA TZEN=1
 
 echo; echo
 echo "Erasing Bank 1"
-${PROG_BIN} -c port=SWD mode=UR --hardRst -ob SECWM1_PSTRT=127 SECWM1_PEND=0 WRP1A_PSTRT=127 WRP1A_PEND=0 WRP1B_PSTRT=127 WRP1B_PEND=0 -e all
+${PROG_BIN} -c port=SWD mode=HotPlug -ob SECWM1_PSTRT=127 SECWM1_PEND=0 WRP1A_PSTRT=127 WRP1A_PEND=0 WRP1B_PSTRT=127 WRP1B_PEND=0 -e all
 
 echo; echo
 echo "Erasing Bank 2"
-${PROG_BIN} -c port=SWD mode=UR --hardRst -ob SECWM2_PSTRT=127 SECWM2_PEND=0 WRP2A_PSTRT=127 WRP2A_PEND=0 WRP2B_PSTRT=127 WRP2B_PEND=0 -e all
+${PROG_BIN} -c port=SWD mode=HotPlug -ob SECWM2_PSTRT=127 SECWM2_PEND=0 WRP2A_PSTRT=127 WRP2A_PEND=0 WRP2B_PSTRT=127 WRP2B_PEND=0 -e all
 
 echo; echo
 echo "Disable HDP protection"
@@ -81,9 +67,13 @@ ${PROG_BIN} -c port=SWD mode=HotPlug -ob HDP1_PEND=0 HDP1EN=0 HDP2_PEND=0 HDP2EN
 
 echo
 echo "Setting Option Byte 1"
-${PROG_BIN} -c port=SWD mode=HotPlug -ob SRAM2_RST=0 SECBOOTADD0="${secbootadd0}" DBANK=1 SWAP_BANK=0 SECWM1_PSTRT=0 SECWM1_PEND=127
+${PROG_BIN} -c port=SWD mode=HotPlug -ob SRAM2_RST=0 SECBOOTADD0=0x0180000 DBANK=1 SWAP_BANK=0 SECWM1_PSTRT=0 SECWM1_PEND=127
 
 echo; echo
 echo "Setting Option Byte 2"
 ${PROG_BIN} -c port=SWD mode=HotPlug -ob SECWM2_PSTRT=0 SECWM2_PEND=127
+
+echo; echo;
+echo "Resetting target"
+${PROG_BIN} -c port=SWD mode=HotPlug --hardRst
 
--- Projects/b_u585i_iot02a_tfm/scripts/flash_device.sh
+++ Projects/b_u585i_iot02a_tfm/scripts/flash_device.sh
@@ -26,57 +26,91 @@
 #
 #
 
-PROG_DIR=`find ${CUBEIDE_DIR%?} -name 'com.st.stm32cube.ide.mcu.externaltools.cubeprogrammer*' -type d`
+# CUBEIDE_DIR, BUILD_PATH, and PROJECT_NAME must be provided by the caller as environment variables
+
+[ -z ${CUBEIDE_DIR} ] && {
+    echo "Error: CUBEIDE_DIR must be defined to continue."
+    exit 1
+}
+
+[ -z ${BUILD_PATH} ] && {
+    echo "Error: BUILD_PATH must be defined to continue."
+    exit 1
+}
+
+[ -z ${PROJECT_NAME} ] && {
+    echo "Error: PROJECT_NAME must be defined to continue."
+    exit 1
+}
+
+PROG_DIR=$( find ${CUBEIDE_DIR} -name 'com.st.stm32cube.ide.mcu.externaltools.cubeprogrammer*' -type d )
 PROG_BIN_DIR="${PROG_DIR}/tools/bin"
-PROG_BIN=`find ${PROG_BIN_DIR} -name 'STM32_Programmer_CLI*' -type f`
+PROG_BIN=$( find ${PROG_BIN_DIR} -name 'STM32_Programmer_CLI*' -type f )
 PROG_BIN=$( basename ${PROG_BIN} )
-TARGET_BIN_DIR=${CWD}
 
 EXT_LOADER="MX25LM51245G_STM32U585I-IOT02A.stldr"
 EXT_LOADER_PATH="${PROG_BIN_DIR}/ExternalLoader/${EXT_LOADER}"
 
 [ -e "${PROG_BIN_DIR}/${PROG_BIN}" ] || {
-    echo "Could not determine STM32CubeProgrammer location"
-    exit -1
+    echo "Error: Failed to determine STM32_Programmer_CLI location."
+    exit 1
 }
 
-[ -e ${TARGET_BIN_DIR} ] || {
-    echo "Project binary dir could not be determined."
-    exit -1
+[ -e "${EXT_LOADER_PATH}" ] || {
+    echo "Error: Failed to find external loader binary."
+    exit 1
 }
 
-source ${TARGET_BIN_DIR}/image_defs.sh
+[ -e ${BUILD_PATH} ] || {
+    echo "Error: Project build directory must be defined in the BUILD_PATH environment variable."
+    exit 1
+}
 
-( [ -z "${slot0}" ] || [ -z "${slot1}" ] || [ -z "${boot}" ] ) && {
+source ${BUILD_PATH}/image_defs.sh
+
+# Valdate required items from image_defs.sh
+( [ -z "${RE_IMAGE_FLASH_ADDRESS_SECURE}" ] || [ -z "${RE_IMAGE_FLASH_ADDRESS_NON_SECURE}" ] || [ -z "${RE_BL2_BOOT_ADDRESS}" ] ) && {
     echo "Error: Could not determine target address."
-    exit -1
+    exit 1
 }
+
+SECBOOTADD0=$(printf "0x%x" $((RE_BL2_BOOT_ADDRESS>>7)))
 
 echo "STM32CubeIDE directory:               ${CUBEIDE_DIR%?}"
 echo "STM32CubeProgrammer Path:             ${PROG_BIN_DIR}"
 echo "STM32CubeProgrammer Bin Name:         ${PROG_BIN}"
 echo; echo
-echo "RE_BL2_BOOT_ADDRESS:                  ${secbootadd0}"
-echo "RE_BL2_PERSO_ADDRESS:                 ${boot}"
-echo "RE_IMAGE_FLASH_ADDRESS_SECURE:        ${slot0}"
-echo "RE_IMAGE_FLASH_ADDRESS_NON_SECURE:    ${slot1}"
-echo "RE_IMAGE_FLASH_SECURE_UPDATE:         ${slot2}"
-echo "RE_IMAGE_FLASH_NON_SECURE_UPDATE:     ${slot3}"
+echo "RE_BL2_BOOT_ADDRESS:                  ${RE_BL2_BOOT_ADDRESS}"
+echo ">> 7 = SECBOOTADD0:                   ${SECBOOTADD0}"
+echo "RE_IMAGE_FLASH_ADDRESS_SECURE:        ${RE_IMAGE_FLASH_ADDRESS_SECURE}"
+echo "RE_IMAGE_FLASH_ADDRESS_NON_SECURE:    ${RE_IMAGE_FLASH_ADDRESS_NON_SECURE}"
 echo
 
 export PATH="${PROG_BIN_DIR}:${PATH}"
 
-echo "Writing TFM Secure image"
-${PROG_BIN} -c port=SWD mode=UR --hardRst -el ${EXT_LOADER_PATH} -d ${TARGET_BIN_DIR}/${ProjName}_s_signed.bin ${slot0} -v
-# ${PROG_BIN} -c port=SWD mode=UR --hardRst -d ${TARGET_BIN_DIR}/${ProjName}_s_signed.bin ${slot0} -v
+echo "Setting TZEN bit and regressing the RDP level to 0."
+${PROG_BIN} -c port=SWD mode=HotPlug -ob RDP=0xAA TZEN=1 || exit
+
+echo "Erasing Internal NOR Flash Bank 1"
+${PROG_BIN} -c port=SWD mode=HotPlug --hardRst -ob SECWM1_PSTRT=127 SECWM1_PEND=0 WRP1A_PSTRT=127 WRP1A_PEND=0 WRP1B_PSTRT=127 WRP1B_PEND=0 -e all || exit
+
+echo "Erasing Internal NOR Flash Bank 2"
+${PROG_BIN} -c port=SWD mode=HotPlug -ob SECWM2_PSTRT=127 SECWM2_PEND=0 WRP2A_PSTRT=127 WRP2A_PEND=0 WRP2B_PSTRT=127 WRP2B_PEND=0 -e all || exit
+
+echo "Disabling Internal NOR flash protection (HDP)"
+${PROG_BIN} -c port=SWD mode=HotPlug -ob HDP1_PEND=0 HDP1EN=0 HDP2_PEND=0 HDP2EN=0 || exit
+
+echo "Setting SECBOOTADD0 option bytes"
+${PROG_BIN} -c port=SWD mode=HotPlug -ob SECBOOTADD0=${SECBOOTADD0} || exit
+
+echo "Writing Secure Image: ${PROJECT_NAME}_s_signed.bin to Address: ${RE_IMAGE_FLASH_ADDRESS_SECURE}"
+${PROG_BIN} -c port=SWD mode=HotPlug -el ${EXT_LOADER_PATH} -d ${BUILD_PATH}/${PROJECT_NAME}_s_signed.bin ${RE_IMAGE_FLASH_ADDRESS_SECURE} -v || exit
 
 echo; echo
-echo "Writing Non-Secure image"
-${PROG_BIN} -c port=SWD mode=UR --hardRst -el ${EXT_LOADER_PATH} -d ${TARGET_BIN_DIR}/${ProjName}_ns_signed.bin ${slot1} -v
-# ${PROG_BIN} -c port=SWD mode=UR --hardRst -d ${TARGET_BIN_DIR}/${ProjName}_ns_signed.bin ${slot1} -v
+echo "Writing Non-Secure Image: ${PROJECT_NAME}_ns_signed.bin to Address: ${RE_IMAGE_FLASH_ADDRESS_NON_SECURE}"
+${PROG_BIN} -c port=SWD mode=HotPlug -el ${EXT_LOADER_PATH} -d ${BUILD_PATH}/${PROJECT_NAME}_ns_signed.bin ${RE_IMAGE_FLASH_ADDRESS_NON_SECURE} -v || exit
 
 echo; echo
-echo "Writing BL2 bootloader image"
-${PROG_BIN} -c port=SWD mode=UR --hardRst -el ${EXT_LOADER_PATH} -d ${TARGET_BIN_DIR}/${ProjName}_bl2.bin ${boot} -v
-# ${PROG_BIN} -c port=SWD mode=UR --hardRst -d ${TARGET_BIN_DIR}/${ProjName}_bl2.bin ${boot} -v
+echo "Writing BL2 Image: ${PROJECT_NAME}_bl2.bin to Address: ${RE_BL2_BIN_ADDRESS}"
+${PROG_BIN} -c port=SWD mode=HotPlug -el ${EXT_LOADER_PATH} -d ${BUILD_PATH}/${PROJECT_NAME}_bl2.bin ${RE_BL2_BIN_ADDRESS} -v || exit
 
