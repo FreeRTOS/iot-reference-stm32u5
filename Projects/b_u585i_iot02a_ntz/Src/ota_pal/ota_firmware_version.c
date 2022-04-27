d14578acaa2c63846f2f153dc60708fc288423c3
Ravishankar Bhagavandas <bhagavar@amazon.com>
2022-04-26 18:33:40 -0700

    Update OTA NTZ initial version

--- Projects/b_u585i_iot02a_ntz/Src/ota_pal/ota_firmware_version.c
+++ Projects/b_u585i_iot02a_ntz/Src/ota_pal/ota_firmware_version.c
@@ -36,8 +36,8 @@
  * rejected in self test phase.1
  */
 
-#define APP_VERSION_MAJOR    0
-#define APP_VERSION_MINOR    9
+#define APP_VERSION_MAJOR    1
+#define APP_VERSION_MINOR    0
 #define APP_VERSION_BUILD    0
 
 
