690f7512ea6cefe0aa1a06f5b1354471c361baf4
Adam Scislowicz <adamds@amazon.com>
2022-04-28 16:38:00 -0700

    add ota e2e configuration example and custom target scripts.

---
+++ TestIntegration/build.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+/home/adamds/st/stm32cubeide_1.9.0/headless-build.sh -data /home/adamds/repos/lab-iot-reference-stm32u5 -build b_u585i_iot02a_ntz/Debug
---
+++ TestIntegration/flash.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+(cd /home/adamds/repos/lab-iot-reference-stm32u5/Projects/b_u585i_iot02a_ntz; \
+ProjDirPath=Debug \
+/home/adamds/repos/lab-iot-reference-stm32u5/tools/stm32u5_tool.sh flash_ntz)
+#ProjDirPath=/home/adamds/repos/lab-iot-reference-stm32u5/Projects/b_u585i_iot02a_ntz \
+
