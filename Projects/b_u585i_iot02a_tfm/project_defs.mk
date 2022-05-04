2688ba71d1172c742431e6fd189fced953569fcf
Paul Bartell <pbartell@amazon.com>
2022-05-03 17:16:04 -0700

    Add project_defs.mk file.

---
+++ Projects/b_u585i_iot02a_tfm/project_defs.mk
@@ -0,0 +1,6 @@
+# S_REGION_SIGNING_KEY = /path/to/my/spe_signing_key-RSA-3072.pem
+# NS_REGION_SIGNING_KEY =  /path/to/my/nspe_signing_key-RSA-3072.pem
+# SPE_VERSION = "1.5.0"
+# NSPE_VERSION = "1.0.0"
+# SPE_SECURITY_COUNTER = 1
+# NSPE_SECURITY_COUNTER = 1
