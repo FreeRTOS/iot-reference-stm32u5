8e5592a926509efe3acb0d904db0a2f9061f67dd
Paul Bartell <pbartell@amazon.com>
2022-05-04 11:51:04 -0700

    Fix shellcheck warnings

--- TestIntegration/build.sh
+++ TestIntegration/build.sh
@@ -1,13 +1,14 @@
-#!/bin/bash
+#!/bin/bash -x
 
-SCRIPT_DIR=`dirname $0`
-PATHTO_STM32CUBEIDE=`which stm32cubeide`
+SCRIPT_DIR=$(dirname "${0}")
+SCRIPT_DIR=$(realpath "${SCRIPT_DIR}")
+PATH_TO_STM32CUBEIDE=$(which stm32cubeide)
 
-if test -z "${PATHTO_STM32CUBEIDE}"; then
+if test -z "${PATH_TO_STM32CUBEIDE}"; then
     echo "ERROR: stm32cubeide must be in your path."
-    exit -1
+    exit 255
 fi
 
-STM32CUBEIDEDIR=`dirname ${PATHTO_STM32CUBEIDE}`
+STM32CUBEIDEDIR=$(dirname "${PATH_TO_STM32CUBEIDE}")
 
-${STM32CUBEIDEDIR}/headless-build.sh -data ${SCRIPT_DIR}/.. -build b_u585i_iot02a_ntz/Debug
+"${STM32CUBEIDEDIR}"/headless-build.sh -data "${SCRIPT_DIR}/.." -build b_u585i_iot02a_ntz
