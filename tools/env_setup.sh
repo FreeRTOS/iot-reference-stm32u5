e72c6035f93c4575cc12aa036311fde0d8082b3a
Paul Bartell <pbartell@amazon.com>
2022-05-25 13:03:49 -0700

    env_setup.sh: Improve compatibility, untabify.

--- tools/env_setup.sh
+++ tools/env_setup.sh
@@ -26,90 +26,147 @@
 #
 
 [ -n "${WORKSPACE_PATH}" ] || {
-	echo "WORKSPACE_PATH env variable was not defined. Defaulting to PWD."
-	WORKSPACE_PATH="${PWD}"
+    echo "WORKSPACE_PATH env variable was not defined. Defaulting to PWD."
+    WORKSPACE_PATH="${PWD}"
 }
 
 TOOLS_PATH="${WORKSPACE_PATH}/tools"
 
 [ -n "${DEBUG}" ] && echo "TOOLS_PATH:         ${TOOLS_PATH}"
 
-VENV_PATH=$(realpath "${TOOLS_PATH}/..")/.venv
+VENV_PATH=$(realpath "${WORKSPACE_PATH}"/.venv)
 [ -n "${DEBUG}" ] && echo "VENV_PATH:          ${VENV_PATH}"
 
-MCUBOOT_PATH=$(realpath "${TOOLS_PATH}/..")/Middleware/ARM/mcuboot
-[ -n "${DEBUG}" ] && echo "MCUBOOT_PATH:       ${MCUBOOT_PATH}"
+MCUBOOT_SCRIPTS=$(realpath "${WORKSPACE_PATH}"/Middleware/ARM/mcuboot/scripts)
+[ -n "${DEBUG}" ] && echo "MCUBOOT_SCRIPTS:    ${MCUBOOT_SCRIPTS}"
 [ -n "${DEBUG}" ] && echo
 
 pycmd="python"
 
-command -v python > /dev/null 2>&1 && pycmd="python"
-command -v python3 > /dev/null 2>&1 && pycmd="python3"
+if command -v python > /dev/null 2>&1; then
+    pycmd="python"
+elif command -v python3 > /dev/null 2>&1; then
+    pycmd="python3"
+fi
 
-command -v ${pycmd} > /dev/null 2>&1 || {
-	echo "Error: Failed to find a valid python installation."
-	return 1
+env_init() {
+    command -v "${pycmd}" > /dev/null 2>&1 || {
+        echo "Error: Failed to find a valid python installation."
+        return 1
+    }
+
+    if command -v winpty > /dev/null 2>&1; then
+        # Determine if shell is interactive
+        if tty -s; then
+            pycmd="winpty ${pycmd}"
+        fi
+    fi
+
+    unset VIRTUAL_ENV
+    VIRTUAL_ENV="${VENV_PATH}"
+
+     OSTYPE=$(uname -o | tr '[:upper:]' '[:lower:]')
+
+    [ -n "${DEBUG}" ] && echo "OSTYPE: ${OSTYPE}"
+
+    CYGPATH_ARG=''
+    if [ "$OSTYPE" = "cygwin" ];then
+        CYGPATH_ARG=''
+    elif [ "$OSTYPE" = "msys" ]; then
+        CYGPATH_ARG='--unix'
+    elif [ "$OSTYPE" = "ms/windows" ]; then
+        CYGPATH_ARG='--mixed'
+    fi
+
+    if [ -n "${CYGPATH_ARG}" ]; then
+        if command -v cygpath &> /dev/null; then
+            VIRTUAL_ENV=$(cygpath "${CYGPATH_ARG}" "${VIRTUAL_ENV}")
+        fi
+    fi
+
+    if [ ! -d "${VENV_PATH}" ]; then
+        echo "Setting up python virtual environment"
+        ${pycmd} -m virtualenv "${VENV_PATH}" || {
+            return 1
+            echo "Virtualenv initialization failed"
+        }
+    fi
+
+    [ -n "${DEBUG}" ] && echo "Activating vritual environment."
+
+
+    if [ -e "${VENV_PATH}/Scripts" ]; then
+        VIRTUALENV_SHIM_DIR="${VENV_PATH}/Scripts"
+    elif [ -e "${VENV_PATH}/bin" ]; then
+        VIRTUALENV_SHIM_DIR="${VENV_PATH}/bin"
+    else
+        echo "Error. Could not find python virtual environment shim directory."
+        return 1
+    fi
+
+    if echo "${PATH}" | grep -qF ';' -; then
+        PATH_SEPARATOR=';'
+    elif echo "${PATH}" | grep -qF ':' -; then
+        PATH_SEPARATOR=':'
+    else
+        echo "Failed to determine path separator from ${PATH}."
+        return 1
+    fi
+
+    SITE_PACKAGES_PATH=$(find "${VENV_PATH}" -name site-packages -type d)
+
+    if [ -n "${CYGPATH_ARG}" ]; then
+        if command -v cygpath &> /dev/null; then
+            SITE_PACKAGES_PATH=$(cygpath "${CYGPATH_ARG}" "${SITE_PACKAGES_PATH}")
+            TOOLS_PATH=$(cygpath "${CYGPATH_ARG}" "${TOOLS_PATH}")
+            MCUBOOT_SCRIPTS=$(cygpath "${CYGPATH_ARG}" "${MCUBOOT_SCRIPTS}")
+            VIRTUAL_ENV=$(cygpath "${CYGPATH_ARG}" "${VIRTUAL_ENV}")
+            VIRTUALENV_SHIM_DIR=$(cygpath "${CYGPATH_ARG}" "${VIRTUALENV_SHIM_DIR}")
+        fi
+    fi
+
+    [ -n "${DEBUG}" ] && echo "SITE_PACKAGES_PATH: ${SITE_PACKAGES_PATH}"
+
+    # Use DEFAULT_PATH as the starting PATH if available
+    if [ -n "${DEFAULT_PATH}" ]; then
+        PATH="${DEFAULT_PATH}"
+    else
+        # Otherwise export the current PATH into DEFAULT_PATH
+        DEFAULT_PATH="${PATH}"
+    fi
+
+    export DEFAULT_PATH
+
+    PATH="${SITE_PACKAGES_PATH}${PATH_SEPARATOR}${PATH}"
+    PATH="${VIRTUALENV_SHIM_DIR}${PATH_SEPARATOR}${PATH}"
+    PATH="${MCUBOOT_SCRIPTS}${PATH_SEPARATOR}${PATH}"
+    PATH="${TOOLS_PATH}${PATH_SEPARATOR}${PATH}"
+
+    export VIRTUAL_ENV
+    export PATH
+    unset PYTHONPATH
+    unset PYTHONHOME
+
+    # .initialized has not been written or requirements.txt / env_setup.sh have been modified
+    if [ ! -e "${VENV_PATH}"/.initialized ] || \
+    [ "${TOOLS_PATH}/requirements.txt" -nt "${VENV_PATH}/.initialized" ] || \
+    [ "${TOOLS_PATH}/env_setup.sh" -nt "${VENV_PATH}/.initialized" ]; then
+        ( ${pycmd} -m pip install --require-virtualenv --upgrade pip &&
+          ${pycmd} -m pip install --require-virtualenv -r "${TOOLS_PATH}"/requirements.txt ) || {
+            rm -rf "${VENV_PATH}"
+            echo "Error while installing requried python packages. Removing incomplete virtual environment."
+            return 1
+        }
+
+        touch "${VENV_PATH}"/.initialized
+    fi
 }
 
+env_init
+
+# shellcheck disable=SC2139
+alias python3="${pycmd}"
 # shellcheck disable=SC2139
 alias python="${pycmd}"
 
-load_venv()
-{
-	if [ -e "${VENV_PATH}"/Scripts ]; then
-		# shellcheck disable=SC1090,SC1091
-		source "${VENV_PATH}"/Scripts/activate
-	elif [ -e "${VENV_PATH}"/bin ]; then
-		# shellcheck disable=SC1090,SC1091
-		source "${VENV_PATH}"/bin/activate
-	else
-		echo "Error. Could not find python virtual environment activation script."
-		return 1
-	fi
-}
-
-if [ ! -d "${VENV_PATH}" ]; then
-	echo "Setting up python virtual environment"
-	python -m virtualenv "${VENV_PATH}" || return 1
-fi
-
-[ -n "${DEBUG}" ] && echo "Activating vritual environment."
-load_venv || return 1
-
-# .initialized has not been written or requirements.txt / env_setup.sh have been modified
-if [[ ! -e "${VENV_PATH}"/.initialized ]] || \
-   [[ "${TOOLS_PATH}/requirements.txt" -nt "${VENV_PATH}/.initialized" ]] || \
-   [[ "${TOOLS_PATH}/env_setup.sh" -nt "${VENV_PATH}/.initialized" ]]; then
-	python -m pip install -r "${TOOLS_PATH}"/requirements.txt || {
-		rm -rf "${VENV_PATH}"
-		echo "Error while installing requried python packages. Removing incomplete virtual environment."
-		return 1
-	}
-
-	SITE_PACKAGES_PATH=$(find "${VENV_PATH}" -name site-packages -type d)
-	echo "SITE_PACKAGES_PATH: ${SITE_PACKAGES_PATH}"
-
-	echo "Adding workspace directory to package path."
-	echo "${WORKSPACE_PATH}" > "${SITE_PACKAGES_PATH}"/workspace.pth
-
-	echo "Adding tools directory to package path."
-	echo "${TOOLS_PATH}" > "${SITE_PACKAGES_PATH}"/tools.pth
-
-	echo "Adding mcuboot scripts directory to package path."
-	echo "${MCUBOOT_PATH}"/scripts > "${SITE_PACKAGES_PATH}"/mcuboot.pth
-
-	touch "${VENV_PATH}"/.initialized
-fi
-
-SITE_PACKAGES_PATH=$(find "${VENV_PATH}" -name site-packages -type d)
-
-# Setup PATH and PYTHONPATH for cmake
-[ -n "${DEBUG}" ] && echo "Adding tools and site packages to PATH."
-export PATH="${MCUBOOT_PATH}/scripts:${TOOLS_PATH}:${SITE_PACKAGES_PATH}:${PATH}"
-
-[ -n "${DEBUG}" ] && echo "Adding tools and site packages to PYTHONPATH."
-export PYTHONPATH="${MCUBOOT_PATH}/scripts:${TOOLS_PATH}:${SITE_PACKAGES_PATH}:${PYTHONPATH}"
-
-[ -n "${DEBUG}" ] && echo "Done."
-
-[ -n "${DEBUG}" ] && exit 0 || return 0
+hash -r 2>/dev/null
