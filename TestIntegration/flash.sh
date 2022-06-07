d4a7f4517121ef5a56c513a94def259cafdc71ab
Paul Bartell <pbartell@amazon.com>
2022-06-07 15:51:57 -0700

    Fix shellcheck warning in flash.sh script.

--- TestIntegration/flash.sh
+++ TestIntegration/flash.sh
@@ -5,6 +5,7 @@ WORKSPACE_DIR=$(realpath "${SCRIPT_DIR}/../")
 
 export ProjDirPath
 ProjDirPath="$(realpath "${WORKSPACE_DIR}/Projects/b_u585i_iot02a_ntz")"
-(cd $ProjDirPath; \
-    BuildArtifactFileBaseName="b_u585i_iot02a_ntz" \
-    "${WORKSPACE_DIR}"/tools/stm32u5_tool.sh flash_ntz)
+pushd "${ProjDirPath}" || exit
+    export BuildArtifactFileBaseName="b_u585i_iot02a_ntz"
+    "${WORKSPACE_DIR}"/tools/stm32u5_tool.sh flash_ntz
+popd || exit
